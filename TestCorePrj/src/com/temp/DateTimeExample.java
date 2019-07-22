698f2bc004bfef5960f9c6c18308b398c87d9752
GSI <GSI@SYS083>
2019-07-22 17:07:21 +0530

    my first commit

---
+++ TestCorePrj/src/com/temp/DateTimeExample.java
@@ -0,0 +1,15 @@
+package com.temp;
+
+import java.util.Date;
+
+public class DateTimeExample {
+	
+	public static void main(String[] args) {
+		String s="000";
+		System.out.println(s.replaceFirst("^0+(?!$)", ""));
+		String l =String.valueOf(Long.parseLong("072"));
+		System.out.println(l);
+		
+	}
+
+}
